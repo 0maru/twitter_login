2889be845c9e66b16023705df20ddf41f0b158d6
maru <0maru.dev@gmail.com>
2020-08-02 19:28:12 +0900

    update comment & refactor

--- lib/schemes/request_header.dart
+++ lib/schemes/request_header.dart
@@ -1,18 +1,30 @@
 import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
+import 'package:twitter_login/src/utils.dart';
+
+const SIGNATURE_METHOD = 'HMAC-SHA1';
 
 class RequestHeader {
+  /// Unique token your application should generate for each unique request.
   final String _nonce;
-  final String _signatureMethod;
+
+  /// The number of seconds since the Unix epoch
   final String _timestamp;
+
+  /// Application identifies
   final String _consumerKey;
+
+  /// Twitter API version
   final String _version;
+
+  /// Access token with your application
   final String _token;
+
+  /// oauth verifier code
   final String _verifier;
 
   String get nonce => _nonce;
-  String get signatureMethod => _signatureMethod;
   String get timestamp => _timestamp;
   String get consumerKey => _consumerKey;
   String get version => _version;
@@ -21,24 +33,23 @@ class RequestHeader {
 
   RequestHeader._({
     @required String nonce,
-    @required String signatureMethod,
     @required String timestamp,
     @required String consumerKey,
     @required String version,
     @required String token,
     @required String verifier,
   })  : this._nonce = nonce,
-        this._signatureMethod = signatureMethod,
         this._timestamp = timestamp,
         this._consumerKey = consumerKey,
         this._version = version,
         this._token = token,
         this._verifier = verifier;
 
+  /// Json serialize
   String serialize() {
     return JsonEncoder().convert({
       'oauth_consumer_key': _consumerKey,
-      'oauth_signature_method': _signatureMethod,
+      'oauth_signature_method': SIGNATURE_METHOD,
       'oauth_signature': '',
       'oauth_timestamp': '',
       'oauth_nonce': _nonce,
@@ -47,36 +58,16 @@ class RequestHeader {
     });
   }
 
-  String authorizeHeader(
-    String apiKey,
-    String signatureDate,
-  ) {
-    final Map<String, String> params = <String, String>{};
-    final int millSeconds = DateTime.now().millisecondsSinceEpoch;
-    params['oauth_nonce'] = millSeconds.toString();
-    params['oauth_signature_method'] = 'HMAC-SHA1';
-    params['oauth_timestamp'] = (millSeconds / 1000).floor().toString();
-    params['oauth_consumer_key'] = apiKey;
-    params['oauth_version'] = '1.0';
-
-    if (!params.containsKey('oauth_signature')) {
-      params['oauth_signature'] = signatureDate;
-    }
-    return 'OAuth ' +
-        params.keys
-            .map((k) => '$k="${Uri.encodeComponent(params[k])}"')
-            .join(', ');
-  }
-
+  /// create requser header for /authorize
   static Map<String, dynamic> authorizeHeaderParams(
     String apiKey,
     String redirectURI,
   ) {
-    final int dtNow = DateTime.now().millisecondsSinceEpoch;
+    final dtNow = DateTime.now().millisecondsSinceEpoch;
     return {
       'oauth_consumer_key': apiKey,
       'oauth_token': '',
-      'oauth_signature_method': 'HMAC-SHA1',
+      'oauth_signature_method': SIGNATURE_METHOD,
       'oauth_timestamp': (dtNow / 1000).floor().toString(),
       'oauth_nonce': dtNow.toString(),
       'oauth_version': '1.0',
@@ -84,16 +75,17 @@ class RequestHeader {
     };
   }
 
+  /// create requser header for /access_token
   static Map<String, dynamic> accessTokenHeaderParams(
     String apiKey,
     String oauthToken,
     String oauthVerifier,
   ) {
-    final int dtNow = DateTime.now().millisecondsSinceEpoch;
+    final dtNow = DateTime.now().millisecondsSinceEpoch;
     return {
       'oauth_consumer_key': apiKey,
       'oauth_token': oauthToken,
-      'oauth_signature_method': 'HMAC-SHA1',
+      'oauth_signature_method': SIGNATURE_METHOD,
       'oauth_timestamp': (dtNow / 1000).floor().toString(),
       'oauth_nonce': dtNow.toString(),
       'oauth_version': '1.0',
