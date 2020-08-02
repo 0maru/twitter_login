5baff586fa8e964dd02d0e08ebec053216b0c62d
maru <0maru.dev@gmail.com>
2020-08-02 18:04:23 +0900

    update comments

--- lib/src/exception.dart
+++ lib/src/exception.dart
@@ -1,3 +1,4 @@
+// Exception thrown when doing user cancelled the login flow.
 class CanceldByUserException implements Exception {
   const CanceldByUserException();
   String toString() => "CanceldByUserException";
