b2dba4cf8517f2b02833f6b8bd885f71bcb77ac5
maru <0maru.dev@gmail.com>
2020-11-03 16:27:30 +0900

    rename

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -13,7 +13,7 @@ const AUTHORIZE_URI = 'https://api.twitter.com/oauth/authorize';
 const ACCESS_TOKEN_URI = 'https://api.twitter.com/oauth/access_token';
 
 ///
-String authHeader(Map<String, dynamic> params) {
+String generateAuthHeader(Map<String, dynamic> params) {
   return 'OAuth ' +
       params.keys.map((k) {
         return '$k="${Uri.encodeComponent(params[k])}"';
@@ -36,7 +36,7 @@ Future<Map<String, dynamic>> send(
       tokenSecretKey: '',
     );
     params['oauth_signature'] = _signature.signatureHmacSha1();
-    final header = authHeader(params);
+    final header = generateAuthHeader(params);
     final http.BaseClient _httpClient = http.Client();
     final http.Response res = await _httpClient.post(
       url,
