9d72d18cfc7973a87be09281c9274137259ec984
maru <0maru.dev@gmail.com>
2020-11-22 23:33:12 +0900

    add return userData

--- lib/src/twitter_login.dart
+++ lib/src/twitter_login.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:twitter_login/entity/user.dart';
 import 'package:twitter_login/schemes/access_token.dart';
 import 'package:twitter_login/entity/auth_result.dart';
 import 'package:twitter_login/schemes/request_token.dart';
@@ -96,12 +97,21 @@ class TwitterLogin {
         apiSecretKey,
         queries,
       );
+
+      final userData = await User.getUserData(
+        apiKey,
+        apiSecretKey,
+        accessToken.authToken,
+        accessToken.authTokenSecret,
+      );
+
       return AuthResult(
         accessToken: accessToken,
         authToken: accessToken.authToken,
         authTokenSecret: accessToken.authTokenSecret,
         status: TwitterLoginStatus.loggedIn,
         errorMessage: '',
+        user: userData,
       );
     } on CanceldByUserException {
       return AuthResult(
@@ -110,6 +120,7 @@ class TwitterLogin {
         authTokenSecret: null,
         status: TwitterLoginStatus.cancelledByUser,
         errorMessage: 'The user cancelled the login flow',
+        user: null,
       );
     } catch (error) {
       return AuthResult(
@@ -118,6 +129,7 @@ class TwitterLogin {
         authTokenSecret: null,
         status: TwitterLoginStatus.error,
         errorMessage: error.toString(),
+        user: null,
       );
     }
   }
