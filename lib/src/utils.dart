1702e78e67ab79a2c7ed3b0002bca5132f2296bd
maru <0maru.dev@gmail.com>
2020-11-22 23:32:32 +0900

    add http get method

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -13,7 +13,8 @@ const AUTHORIZE_URI = 'https://api.twitter.com/oauth/authorize';
 const ACCESS_TOKEN_URI = 'https://api.twitter.com/oauth/access_token';
 
 /// The "Request email addresses from users"
-const ACCOUNT_VERIFY_URI = 'https://api.twitter.com/1.1/account/verify_credentials.json';
+const ACCOUNT_VERIFY_URI =
+    'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true';
 
 ///
 String generateAuthHeader(Map<String, dynamic> params) {
@@ -42,6 +43,38 @@ Future<Map<String, dynamic>> send(
     final header = generateAuthHeader(params);
     final http.BaseClient _httpClient = http.Client();
     final http.Response res = await _httpClient.post(
+      url,
+      headers: <String, String>{'Authorization': header},
+    );
+    if (res.statusCode != 200) {
+      throw HttpException("Failed ${res.reasonPhrase}");
+    }
+
+    return Uri.splitQueryString(res.body);
+  } on Exception catch (error) {
+    throw Exception(error);
+  }
+}
+
+Future<Map<String, dynamic>> httpGet(
+  String url,
+  Map<String, dynamic> params,
+  String apiKey,
+  String apiSecretKey,
+  String tokenSecret,
+) async {
+  try {
+    final _signature = Signature(
+      url: url,
+      params: params,
+      apiKey: apiKey,
+      apiSecretKey: apiSecretKey,
+      tokenSecretKey: tokenSecret,
+    );
+    params['oauth_signature'] = _signature.signatureHmacSha1();
+    final header = generateAuthHeader(params);
+    final http.BaseClient _httpClient = http.Client();
+    final http.Response res = await _httpClient.get(
       url,
       headers: <String, String>{'Authorization': header},
     );
