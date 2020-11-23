31df2e58928c023fc191815d7290ec97f2a322ea
Toi Yoshimaru <43493921+0maru@users.noreply.github.com>
2020-11-23 16:11:17 +0900

    Revert "feature account/verify_credentials"

--- lib/schemes/auth_result.dart
+++ lib/schemes/auth_result.dart
@@ -1,5 +1,4 @@
 import 'dart:core';
-import 'package:twitter_login/entity/user.dart';
 import 'package:twitter_login/schemes/access_token.dart';
 import 'package:twitter_login/src/twitter_login.dart';
 
@@ -21,16 +20,12 @@ class AuthResult {
   /// The error message when the log in flow completed with an error
   final String _errorMessage;
 
-  /// Twitter Account user Info.
-  final User _user;
-
   @deprecated
   AccessToken get accessToken => _accessToken;
   String get authToken => _authToken;
   String get authTokenSecret => _authTokenSecret;
   TwitterLoginStatus get status => _status;
   String get errorMessage => _errorMessage;
-  User get user => _user;
 
   /// constractor
   AuthResult({
@@ -39,11 +34,9 @@ class AuthResult {
     String authTokenSecret,
     TwitterLoginStatus status,
     String errorMessage,
-    User user,
   })  : this._accessToken = accessToken,
         this._authToken = authToken,
         this._authTokenSecret = authTokenSecret,
         this._status = status,
-        this._errorMessage = errorMessage,
-        this._user = user;
+        this._errorMessage = errorMessage;
 }
--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart' as http;
@@ -13,10 +12,6 @@ const AUTHORIZE_URI = 'https://api.twitter.com/oauth/authorize';
 /// Allows a Consumer application to use an OAuth request_token to request user authorization.
 const ACCESS_TOKEN_URI = 'https://api.twitter.com/oauth/access_token';
 
-/// The "Request email addresses from users"
-const ACCOUNT_VERIFY_URI =
-    'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true';
-
 ///
 String generateAuthHeader(Map<String, dynamic> params) {
   return 'OAuth ' +
@@ -26,7 +21,7 @@ String generateAuthHeader(Map<String, dynamic> params) {
 }
 
 /// send http request
-Future<Map<String, dynamic>> httpPost(
+Future<Map<String, dynamic>> send(
   String url,
   Map<String, dynamic> params,
   String apiKey,
@@ -55,61 +50,4 @@ Future<Map<String, dynamic>> httpPost(
   } on Exception catch (error) {
     throw Exception(error);
   }
-}
-
-Future<Map<String, dynamic>> httpGet(
-  String url,
-  Map<String, dynamic> params,
-  String apiKey,
-  String apiSecretKey,
-  String tokenSecret,
-) async {
-  try {
-    final _signature = Signature(
-      url: url,
-      method: 'GET',
-      params: params,
-      apiKey: apiKey,
-      apiSecretKey: apiSecretKey,
-      tokenSecretKey: tokenSecret,
-    );
-    params['oauth_signature'] = _signature.signatureHmacSha1();
-    final header = generateAuthHeader(params);
-    final http.BaseClient _httpClient = http.Client();
-    final http.Response res = await _httpClient.get(
-      url,
-      headers: <String, String>{'Authorization': header},
-    );
-    if (res.statusCode != 200) {
-      throw HttpException("Failed ${res.reasonPhrase}");
-    }
-
-    return jsonDecode(res.body);
-  } on Exception catch (error) {
-    throw Exception(error);
-  }
-}
-
-Map<String, String> requestHeader({
-  String apiKey,
-  String oauthToken = '',
-  String redirectURI = '',
-  String oauthVerifier = '',
-}) {
-  final dtNow = DateTime.now().millisecondsSinceEpoch;
-  final params = {
-    'oauth_consumer_key': apiKey,
-    'oauth_token': oauthToken,
-    'oauth_signature_method': 'HMAC-SHA1',
-    'oauth_timestamp': (dtNow / 1000).floor().toString(),
-    'oauth_nonce': dtNow.toString(),
-    'oauth_version': '1.0',
-  };
-  if (redirectURI.isNotEmpty ?? true) {
-    params.addAll({'oauth_callback': redirectURI});
-  }
-  if (oauthVerifier.isNotEmpty ?? true) {
-    params.addAll({'oauth_verifier': oauthVerifier});
-  }
-  return params;
 }
