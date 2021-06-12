460a8ba3ea8b5d2088ebf6e82539b6a602ab39ee
0maru <0maru.dev@gmail.com>
2021-06-12 20:35:39 +0900

    update sample code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:twitter_login/twitter_login.dart';
+import 'package:twitter_login_example/env.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,44 +19,62 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: TextButton(
-            child: Text('login'),
-            onPressed: () async {
-              final twitterLogin = TwitterLogin(
-                // Consumer API keys
-                apiKey: 'xxx',
-                // Consumer API Secret keys
-                apiSecretKey: 'xxxxx',
-                // Registered Callback URLs in TwitterApp
-                // Android is a deeplink
-                // iOS is a URLScheme
-                redirectURI: 'example://',
-                // Forces the user to enter their credentials
-                // to ensure the correct users account is authorized.
-              );
-              // If you want to implement Twitter account switching, set [force_login] to true
-              // login(forceLogin: true);
-              final authResult = await twitterLogin.login();
-              switch (authResult.status) {
-                case TwitterLoginStatus.loggedIn:
-                  // success
-                  print('====== Login success ======');
-                  break;
-                case TwitterLoginStatus.cancelledByUser:
-                  // cancel
-                  print('====== Login cancel ======');
-                  break;
-                case TwitterLoginStatus.error:
-                case null:
-                  // error
-                  print('====== Login error ======');
-                  break;
-              }
-            },
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: TextButton(
+                child: Text('login'),
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.blueAccent),
+                  minimumSize: MaterialStateProperty.all<Size>(Size(160, 48)),
+                ),
+                onPressed: () async {
+                  await login();
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  Future login() async {
+    final twitterLogin = TwitterLogin(
+      /// Consumer API keys
+      apiKey: API_KEY,
+
+      /// Consumer API Secret keys
+      apiSecretKey: API_SECRET_KEY,
+
+      /// Registered Callback URLs in TwitterApp
+      /// Android is a deeplink
+      /// iOS is a URLScheme
+      redirectURI: 'example://',
+    );
+
+    /// Forces the user to enter their credentials
+    /// to ensure the correct users account is authorized.
+    /// If you want to implement Twitter account switching, set [force_login] to true
+    /// login(forceLogin: true);
+    final authResult = await twitterLogin.login();
+    switch (authResult.status) {
+      case TwitterLoginStatus.loggedIn:
+        // success
+        print('====== Login success ======');
+        break;
+      case TwitterLoginStatus.cancelledByUser:
+        // cancel
+        print('====== Login cancel ======');
+        break;
+      case TwitterLoginStatus.error:
+      case null:
+        // error
+        print('====== Login error ======');
+        break;
+    }
   }
 }
