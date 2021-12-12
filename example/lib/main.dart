134946941bb2f2ba79f7134f0e0c38ff087b1968
0maru <0maru.dev@gmail.com>
2021-12-12 23:26:06 +0900

    feat: update example code.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,32 +16,60 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('twitter_login example app'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-              child: TextButton(
-                child: Text('login'),
-                style: ButtonStyle(
-                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.blueAccent),
-                  minimumSize: MaterialStateProperty.all<Size>(Size(160, 48)),
-                ),
-                onPressed: () async {
-                  await login();
-                },
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: ListView(
+            children: [
+              SizedBox(height: 24),
+              Text(
+                'Twitter API v1.1 is not available when creating a new application in twitter developer from November 15, 2021.\n'
+                'Check the Twitter Developer to see if it supports v1.1 or v2.',
               ),
-            ),
-          ],
+              SizedBox(height: 24),
+              Image.asset(
+                'assets/twitter_dashboard.png',
+                width: double.infinity,
+              ),
+              SizedBox(height: 24),
+              Center(
+                child: TextButton(
+                  child: Text('use Twitter API v1.1'),
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.blueAccent),
+                    minimumSize: MaterialStateProperty.all<Size>(Size(160, 48)),
+                  ),
+                  onPressed: () async {
+                    await login();
+                  },
+                ),
+              ),
+              SizedBox(height: 24),
+              Center(
+                child: TextButton(
+                  child: Text('use Twitter API v2.0'),
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.blueAccent),
+                    minimumSize: MaterialStateProperty.all<Size>(Size(160, 48)),
+                  ),
+                  onPressed: () async {
+                    await loginV2();
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
+  /// Use Twitter API v1.1
   Future login() async {
     final twitterLogin = TwitterLogin(
       /// Consumer API keys
@@ -61,6 +89,43 @@ class _MyAppState extends State<MyApp> {
     /// If you want to implement Twitter account switching, set [force_login] to true
     /// login(forceLogin: true);
     final authResult = await twitterLogin.login();
+    switch (authResult.status) {
+      case TwitterLoginStatus.loggedIn:
+        // success
+        print('====== Login success ======');
+        break;
+      case TwitterLoginStatus.cancelledByUser:
+        // cancel
+        print('====== Login cancel ======');
+        break;
+      case TwitterLoginStatus.error:
+      case null:
+        // error
+        print('====== Login error ======');
+        break;
+    }
+  }
+
+  /// Use Twitter API v2.
+  Future loginV2() async {
+    final twitterLogin = TwitterLogin(
+      /// Consumer API keys
+      apiKey: API_KEY,
+
+      /// Consumer API Secret keys
+      apiSecretKey: API_SECRET_KEY,
+
+      /// Registered Callback URLs in TwitterApp
+      /// Android is a deeplink
+      /// iOS is a URLScheme
+      redirectURI: 'example://',
+    );
+
+    /// Forces the user to enter their credentials
+    /// to ensure the correct users account is authorized.
+    /// If you want to implement Twitter account switching, set [force_login] to true
+    /// login(forceLogin: true);
+    final authResult = await twitterLogin.loginV2();
     switch (authResult.status) {
       case TwitterLoginStatus.loggedIn:
         // success
