d5fc1cb06dd4ecc849608af1b986cdeead10d737
0maru <0maru.dev@gmail.com>
2022-04-16 12:49:41 +0900

    update flutter install script

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -2,5 +2,9 @@
 
 BRANCH=$1
 
+cd $HOME
 git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH _flutter
-echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH
+echo "$HOME/_flutter/bin" >> $GITHUB_PATH
+echo "$HOME/.pub-cache/bin" >> $GITHUB_PATH
+echo "$HOME/_flutter/.pub-cache/bin" >> $GITHUB_PATH
+echo "$HOME/_flutter/bin/cache/dart-sdk/bin" >> $GITHUB_PATH
