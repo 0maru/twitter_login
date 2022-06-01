0d4c3b1534895b239c6a5e806279a5d1b97df4bd
Konstantin Adamov <rayadams@outlook.com>
2022-06-01 09:59:52 -0700

    Code format

--- macos/Classes/TwitterLoginPlugin.swift
+++ macos/Classes/TwitterLoginPlugin.swift
@@ -12,49 +12,53 @@ public class TwitterLoginPlugin: NSObject, FlutterPlugin, ASWebAuthenticationPre
         return NSApplication.shared.mainWindow!
     }
     
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "twitter_login/auth_browser", binaryMessenger: registrar.messenger)
-    let instance = TwitterLoginPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "twitter_login/auth_browser", binaryMessenger: registrar.messenger)
+        let instance = TwitterLoginPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+    
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-            case "authentication":
-        if #available(macOS 10.15, *) {
-            authentication(call, result: result)
-        } else {
-            // Fallback on earlier versions
-        }
-            default:
+        switch call.method {
+        case "authentication":
+            if #available(macOS 10.15, *) {
+                authentication(call, result: result)
+            } else {
                 result(nil)
                 return
+            }
+        default:
+            result(nil)
+            return
         }
-  }
-    @available(macOS 10.15, *)
+    }
+    
+    
+    @available(macOS 10.15, *)  //required by XCode or fail to compile
     public func authentication(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let args = call.arguments as! NSDictionary
         let url = args["url"] as! String
         let urlScheme = args["redirectURL"] as? String
         
         
-            var authSession: ASWebAuthenticationSession?
-            authSession = ASWebAuthenticationSession(
-                url: URL(string: url)!,
-                callbackURLScheme: urlScheme
-            ) { url, error in
-                result(url?.absoluteString)
-                authSession!.cancel()
-                self.session = nil
-            }
-            self.session = authSession
-            if #available(iOS 13.0, *) {
-                authSession?.presentationContextProvider = self
-            }
-            if !authSession!.start() {
+        var authSession: ASWebAuthenticationSession?
+        authSession = ASWebAuthenticationSession(
+            url: URL(string: url)!,
+            callbackURLScheme: urlScheme
+        ) { url, error in
+            result(url?.absoluteString)
+            authSession!.cancel()
+            self.session = nil
+        }
+        self.session = authSession
+        if #available(iOS 13.0, *) {
+            authSession?.presentationContextProvider = self
+        }
+        if !authSession!.start() {
             // TODO: failed
-            }
+            result(nil)
+        }
         
     }
 }
