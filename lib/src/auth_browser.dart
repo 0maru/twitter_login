14a70aec5d78f91a1b2c0df1aaec3c6b2edd57ba
0maru <0maru.dev@gmail.com>
2023-07-30 23:57:29 +0900

    fix: VoidCallback to void Function()

--- lib/src/auth_browser.dart
+++ lib/src/auth_browser.dart
@@ -17,7 +17,7 @@ class AuthBrowser {
   static const _channel = MethodChannel(methodName);
   late MethodChannel _methodCallHandlerChannel;
   bool _isOpen = false;
-  VoidCallback onClose;
+  void Function() onClose;
 
   Future<void> methodCallHandler(MethodCall call) async {
     switch (call.method) {
